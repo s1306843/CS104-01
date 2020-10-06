0c9214c061c95bd1e8d4ae7cc79d10faa11e95b0
s1306843 <71726066+s1306843@users.noreply.github.com>
2020-10-05 21:09:42 -0400

    First commit

---
+++ conditions.py
@@ -0,0 +1,20 @@
+temp=0
+
+temp=int(input("Please enter the current temperature: "))
+
+if temp > 90:
+    print ("Wear Shorts")
+elif temp > 70:
+    print ("Short sleeves are fine")
+elif temp > 50:
+    print ("Wear a jacket")
+elif temp > 32:
+    print ("Wear a heavy coat")
+else:
+    print ("Stay inside")
+
+
+#Connor Przelomski                                        10/6/2020
+
+
+    
